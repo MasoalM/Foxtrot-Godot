d78fb203763b06906f360cd727ef6898014d2247
MasoalM <105206059+MasoalM@users.noreply.github.com>
2026-04-03 19:20:25 +0200

    Animaciones Wolfior acabadas

--- Enemigos/wolfior.gd
+++ Enemigos/wolfior.gd
@@ -58,194 +58,196 @@ func _ready():
 
 
 func _physics_process(delta):
-	attack_timer -= delta
-	
-	# --- KNOCKBACK ---
-	if knockback_timer > 0:
-		knockback_timer -= delta
-	else:
-		in_knockback = false
-
-	# --- GRAVEDAD ---
-	if !is_on_floor():
-		velocity.y += gravity * delta
-
-	# --- DETECCIÓN PLAYER ---
-	if player:
-		var distance = global_position.distance_to(player.global_position)
-
-		if can_see_player() and distance < detect_distance:
-			chasing = true
-			chase_timer = chase_duration
-
-	# --- CHASE ---
-	if chasing:
-		animated_sprite.play("chase")
-		chase_timer -= delta
-
-		if is_instance_valid(player):
-			var distance_x = abs(player.global_position.x - global_position.x)
-
-			if not in_knockback:
-				if distance_x < attack_distance:
-					velocity.x = 0
-					if attack_timer <= 0:
-						attack()
-						attack_timer = attack_cooldown
-				else:
-					if player.global_position.x > global_position.x:
-						velocity.x = speed
-					else:
-						velocity.x = -speed
-
-			if chase_timer <= 0:
-				chasing = false
-
-	# --- PATROL ---
-	else:
-		animated_sprite.play("walk")
-		patrol_wait_time -= delta
-
-		if is_waiting:
-			if not in_knockback:
-				velocity.x = 0
-
-			if patrol_wait_time <= 0:
-				is_waiting = false
-				patrol_wait_time = patrol_move_duration
-
+	if lives > 0:
+		attack_timer -= delta
+		
+		# --- KNOCKBACK ---
+		if knockback_timer > 0:
+			knockback_timer -= delta
 		else:
-			if not in_knockback:
-				velocity.x = patrol_direction * speed
+			in_knockback = false
 
-			if global_position.x > start_position.x + patrol_distance:
-				patrol_direction = -1
+		# --- GRAVEDAD ---
+		if !is_on_floor():
+			velocity.y += gravity * delta
 
-			if global_position.x < start_position.x - patrol_distance:
-				patrol_direction = 1
+		# --- DETECCIÓN PLAYER ---
+		if player:
+			var distance = global_position.distance_to(player.global_position)
 
-			if patrol_wait_time <= 0:
-				is_waiting = true
-				patrol_wait_time = patrol_wait_duration
+			if can_see_player() and distance < detect_distance:
+				chasing = true
+				chase_timer = chase_duration
 
-	# --- FLIP GLOBAL + ESPADA ---
-	var flip = false
+		# --- CHASE ---
+		if chasing:
+			if is_on_floor():
+				animated_sprite.play("chase")
+			chase_timer -= delta
 
-	if chasing and player:
-		flip = player.global_position.x < global_position.x
-	elif velocity.x != 0:
-		flip = velocity.x < 0
+			if is_instance_valid(player):
+				var distance_x = abs(player.global_position.x - global_position.x)
 
-	# Flip del lobo
-	$AnimatedSprite2D.flip_h = flip
+				if not in_knockback:
+					if distance_x < attack_distance:
+						velocity.x = 0
+						if attack_timer <= 0:
+							attack()
+							attack_timer = attack_cooldown
+					else:
+						if player.global_position.x > global_position.x:
+							velocity.x = speed
+						else:
+							velocity.x = -speed
 
-	# Flip de la espada (sprite interno)
-	espada_sprite.flip_h = flip
+				if chase_timer <= 0:
+					chasing = false
 
-	if flip:
-		# IZQUIERDA
-		espada_idle.position.x = -40
-		espada_idle.rotation = deg_to_rad(-30)
-	else:
-		# DERECHA
-		espada_idle.position.x = 35
-		espada_idle.rotation = deg_to_rad(30)
+		# --- PATROL ---
+		else:
+			patrol_wait_time -= delta
 
-	# --- ANTI-STUCK ---
-	if abs(global_position.x - last_x) < 1 and is_on_floor() and velocity.x != 0:
-		stuck_timer += delta
-	else:
-		stuck_timer = 0
+			if is_waiting:
+				if is_on_floor():
+					animated_sprite.play("idle")
+				if not in_knockback:
+					velocity.x = 0
 
-	if stuck_timer > stuck_time_limit:
-		velocity.y = jump_force
-		stuck_timer = 0
+				if patrol_wait_time <= 0:
+					is_waiting = false
+					if is_on_floor():
+						animated_sprite.play("walk")
+					patrol_wait_time = patrol_move_duration
 
-	last_x = global_position.x
+			else:
+				if not in_knockback:
+					velocity.x = patrol_direction * speed
 
-	move_and_slide()
+				if global_position.x > start_position.x + patrol_distance:
+					patrol_direction = -1
+
+				if global_position.x < start_position.x - patrol_distance:
+					patrol_direction = 1
+
+				if patrol_wait_time <= 0:
+					is_waiting = true
+					patrol_wait_time = patrol_wait_duration
+
+		# --- FLIP GLOBAL + ESPADA ---
+		var flip = false
+
+		if chasing and player:
+			flip = player.global_position.x < global_position.x
+		elif velocity.x != 0:
+			flip = velocity.x < 0
+
+		# Flip del lobo
+		$AnimatedSprite2D.flip_h = flip
+
+		# Flip de la espada (sprite interno)
+		espada_sprite.flip_h = flip
+
+		if flip:
+			# IZQUIERDA
+			espada_idle.position.x = -40
+			espada_idle.rotation = deg_to_rad(-30)
+		else:
+			# DERECHA
+			espada_idle.position.x = 35
+			espada_idle.rotation = deg_to_rad(30)
+
+		# --- ANTI-STUCK ---
+		if abs(global_position.x - last_x) < 1 and is_on_floor() and velocity.x != 0:
+			stuck_timer += delta
+		else:
+			stuck_timer = 0
+
+		if (stuck_timer > stuck_time_limit):
+			animated_sprite.play("jump")
+			velocity.y = jump_force
+			stuck_timer = 0
+
+		last_x = global_position.x
+
+		move_and_slide()
 	
 func can_see_player():
-	if player == null or ray == null:
+	if lives > 0:
+		if player == null or ray == null:
+			return false
+
+		var origin = global_position + Vector2(0, -20)
+		var target = player.global_position + Vector2(0, -20)
+
+		var direction = target - origin
+		ray.global_position = origin
+		ray.target_position = direction.normalized() * 800
+		ray.force_raycast_update()
+
+		if ray.is_colliding():
+			var collider = ray.get_collider()
+			if collider.is_in_group("player"):
+				return true
+			return false
 		return false
-
-	var origin = global_position + Vector2(0, -20)
-	var target = player.global_position + Vector2(0, -20)
-
-	var direction = target - origin
-	ray.global_position = origin
-	ray.target_position = direction.normalized() * 800
-	ray.force_raycast_update()
-
-	if ray.is_colliding():
-		var collider = ray.get_collider()
-		if collider.is_in_group("player"):
-			return true
-
-		
-		return false
-
-	
 	return false
 	
 	
 func attack():
-	if !is_instance_valid(player):
-		return
+	if lives > 0:
+		if !is_instance_valid(player):
+			return
 
-	var espada = espada_scene.instantiate()
+		var espada = espada_scene.instantiate()
 
-	var offset_x = 20
-	var dir = 1
+		var offset_x = 20
+		var dir = 1
 
-	# Detectar dirección del jugador
-	if player.global_position.x < global_position.x:
-		offset_x = -20
-		dir = -1
-	
-	var pos = global_position + Vector2(offset_x, 20)
+		# Detectar dirección del jugador
+		if player.global_position.x < global_position.x:
+			offset_x = -20
+			dir = -1
+		
+		var pos = global_position + Vector2(offset_x, 20)
 
-	if is_instance_valid(espada_idle):
-		espada_idle.visible = false
+		if is_instance_valid(espada_idle):
+			espada_idle.visible = false
 
-	# Configurar espada de ataque
-	espada.global_position = pos
-	espada.scale.x = dir
+		# Configurar espada de ataque
+		espada.global_position = pos
+		espada.scale.x = dir
 
-	if espada.has_method("set_direction"):
-		espada.set_direction(dir)
+		if espada.has_method("set_direction"):
+			espada.set_direction(dir)
 
-	get_parent().add_child(espada)
+		get_parent().add_child(espada)
 
-	# --- HITBOX ---
-	hitbox.monitoring = true
+		# --- HITBOX ---
+		hitbox.monitoring = true
 
-	var shape_node = hitbox.get_node("CollisionShape2D")
-	var shape = shape_node.shape as CapsuleShape2D
+		var shape_node = hitbox.get_node("CollisionShape2D")
+		var shape = shape_node.shape as CapsuleShape2D
 
-	var original_radius = shape.radius
-	var original_height = shape.height
+		var original_radius = shape.radius
+		var original_height = shape.height
 
-	# Agrandar hitbox
-	shape.radius = original_radius * 2
+		# Agrandar hitbox
+		shape.radius = original_radius * 2
 
-	# Mover hitbox según dirección
-	if dir == -1:
-		shape_node.position.x = -original_radius
-	else:
-		shape_node.position.x = original_radius
+		# Mover hitbox según dirección
+		if dir == -1:
+			shape_node.position.x = -original_radius
+		else:
+			shape_node.position.x = original_radius
 
-	await get_tree().create_timer(0.25).timeout
+		await get_tree().create_timer(0.25).timeout
 
-	# Restaurar hitbox
-	shape.radius = original_radius
-	shape.height = original_height
-	shape_node.position.x = 0
-	
-
-
-	if is_instance_valid(espada_idle):
-		espada_idle.visible = true
+		# Restaurar hitbox
+		shape.radius = original_radius
+		shape.height = original_height
+		shape_node.position.x = 0
+		if is_instance_valid(espada_idle):
+			espada_idle.visible = true
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print(area.get_groups())
@@ -268,5 +270,6 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		
 		
 		if lives == 0:
-			print("liberado")
+			animated_sprite.play("death")
+			await get_tree().create_timer(1.5).timeout
 			queue_free()
