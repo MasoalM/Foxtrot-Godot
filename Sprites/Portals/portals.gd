06b1735c274051895e0cff2f45506815ee6957c4
MasoalM <105206059+MasoalM@users.noreply.github.com>
2026-04-09 12:15:28 +0200

    Sale tecla F al estar en un portal

--- Sprites/Portals/portals.gd
+++ Sprites/Portals/portals.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @export var escena_destino: String = ""
-
 var jugador_dentro = false
 
+# Referencia al Sprite2D que mostrará la "F"
+@onready var indicador: Sprite2D = $Indicador
+
 func _ready() -> void:
-	pass
+	indicador.visible = false  # Oculto al inicio
 
 func _process(delta: float) -> void:
 	if jugador_dentro and Input.is_action_just_pressed("enter_portal"):
@@ -18,12 +20,13 @@ func entrar_al_nivel():
 	print("Entrando al nivel: ", escena_destino)
 	get_tree().change_scene_to_file(escena_destino)
 
-# Para el CharacterBody2D del jugador
 func _on_area_2d_body_entered(body):
 	print("body entró: ", body.name)
 	if body.is_in_group("player"):
 		jugador_dentro = true
+		indicador.visible = true  # Mostrar imagen
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("player"):
 		jugador_dentro = false
+		indicador.visible = false  # Ocultar imagen
