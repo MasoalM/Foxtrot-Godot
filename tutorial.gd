f184db0111fbab9ef5608b99aeb3bbd8be09ce58
MasoalM <105206059+MasoalM@users.noreply.github.com>
2026-04-09 18:22:33 +0200

    Tutorial lvl 1

---
+++ tutorial.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var indicador: Sprite2D = $Indicador
+
+func _ready() -> void:
+	indicador.visible = false  # Oculto al inicio
+
+func _on_area_2d_body_entered(body):
+	print("body entró: ", body.name)
+	if body.is_in_group("player"):
+		indicador.visible = true  # Mostrar imagen
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("player"):
+		indicador.visible = false  # Ocultar imagen
