009d55ad8b8477be604c408ece175c6bb7f763e4
hugoBackup <hugovalls082@gmail.com>
2026-04-09 23:27:05 +0200

    hide hitboxes

--- Enemigos/wolfior.gd
+++ Enemigos/wolfior.gd
@@ -55,7 +55,7 @@ const DEATH_LIVES = 0
 
 
 func _ready():
-	get_tree().debug_collisions_hint = true
+	get_tree().debug_collisions_hint = false
 	
 	add_to_group("Enemigos")
 	player = get_tree().get_first_node_in_group("player")
