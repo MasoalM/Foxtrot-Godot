058f21568bc8f938e99897040a3811446a76c8cc
MasoalM <105206059+MasoalM@users.noreply.github.com>
2026-04-23 01:30:58 +0200

    V0.6.3
    Animación Mox menú. Click en Mox. Nuevos efectos de sonido

--- Menús/escenas/botones_principal.gd
+++ Menús/escenas/botones_principal.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 var pointer_texture = preload("res://Sprites/Pointer.png")
+@onready var sprite = $"../Mox Nose/AnimatedSprite2D"
 
 func _ready():
 	Input.set_custom_mouse_cursor(pointer_texture, Input.CURSOR_ARROW)
@@ -38,7 +39,29 @@ func _on_salir_pressed() -> void:
 	get_tree().quit()
 
 
-# -- H  --
+var shaking := false
 
 func _on_mox_nose_pressed() -> void:
-	AudioManager.play("MoxNose", 0.2)
+	if shaking:
+		return
+
+	shaking = true
+	AudioManager.play("MadMox", 0.2)
+	sprite.scale = Vector2(3.5, 3.5)
+	await get_tree().create_timer(0.08).timeout
+	sprite.scale = Vector2(3, 3)
+
+	var original_pos = sprite.position
+	sprite.modulate = Color(1, 0.4, 0.4)
+
+	for i in 6:
+		sprite.position = original_pos + Vector2(
+			randf_range(-3, 3),
+			randf_range(-3, 3)
+		)
+		await get_tree().create_timer(0.03).timeout
+
+	sprite.position = original_pos
+	sprite.modulate = Color(1, 1, 1)
+
+	shaking = false
