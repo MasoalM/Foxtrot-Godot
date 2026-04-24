87537235facb946ecfdd78cbc9e78407f7e41c67
MasoalM <105206059+MasoalM@users.noreply.github.com>
2026-04-24 13:56:24 +0200

    V0.6.6

--- niveles/camera.gd
+++ niveles/camera.gd
@@ -1,4 +1,9 @@
 extends Camera2D
 
+@export var offset_x := 400
+
 func _process(delta):
-	global_position = get_parent().global_position.round()
+	var target_pos = get_parent().global_position
+	target_pos.x += offset_x
+	
+	global_position = target_pos.round()
