d6b08f7d710e082e62474e29d3a049239b2b34da
MasoalM <105206059+MasoalM@users.noreply.github.com>
2026-05-05 19:22:17 +0200

    Cambio icono y retoques
    -Icono Mox
    -Código torreta arreglado
    -Iluminación torreta y flechas
    -Color lava nivel 5 y boss fight arreglado

--- Enemigos/torreta.gd
+++ Enemigos/torreta.gd
@@ -1,25 +1,32 @@
 extends RigidBody2D
-
 const proyectil = preload("res://Proyectiles/proyectilTorreta.tscn")
 @onready var shotSound = $AudioStreamPlayer2DShot
+@onready var pointLight = $PointLight2D
 
 const shootCooldown = 100
 var shootTime = shootCooldown
-# Called when the node enters the scene tree for the first time.
+
+# Configuración de la luz pulsante
+const LIGHT_MIN = 1
+const LIGHT_MAX = 3
+const PULSE_SPEED = 0.5  # ciclos por segundo
+var pulse_time = 0.0
+
 func _ready() -> void:
 	randomize()
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if shootTime<=0:
+	# Disparo independiente de FPS
+	shootTime -= delta * 60  # Convierte el cooldown a segundos (asume 60fps base)
+	if shootTime <= 0:
 		var shoot = proyectil.instantiate()
-		#shotSound.play()
 		get_parent().add_child(shoot)
 		shoot.position = $Marker2D.global_position
 		shoot.scale.x *= -1
 		shoot.vel_bala *= -1
-		shootTime=shootCooldown*randf_range(0.5,1.5)
-	else:
-		shootTime-=1
+		shootTime = shootCooldown * randf_range(0.5, 1.5)
+
+	# Luz pulsante con delta (independiente de FPS)
+	pulse_time += delta * PULSE_SPEED
+	var t = (sin(pulse_time * TAU) + 1.0) / 2.0  # oscila entre 0.0 y 1.0
+	pointLight.energy = lerp(LIGHT_MIN, LIGHT_MAX, t)
