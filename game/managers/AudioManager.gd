6afea1c075c4d7347a6bce4129f65756d8e4de2e
hugoBackup <hugovalls082@gmail.com>
2026-05-12 13:00:58 +0200

    sonidos arreglados en el exe

--- game/managers/AudioManager.gd
+++ game/managers/AudioManager.gd
@@ -6,6 +6,48 @@ const AUDIO_PATHS = [
 	"res://Music/"
 ]
 
+const PRELOAD_SOUNDS = {
+	"BOSS": preload("res://Music/BOSS.ogg"),
+	"BossFightAntigua": preload("res://Music/BossFightAntigua.ogg"),
+	"DeathScreen": preload("res://Music/DeathScreen.ogg"),
+	"GameEnding": preload("res://Music/GameEnding.ogg"),
+	"Level1": preload("res://Music/Level1.ogg"),
+	"Level2": preload("res://Music/Level2.ogg"),
+	"Level3": preload("res://Music/Level3.ogg"),
+	"Level4": preload("res://Music/Level4.ogg"),
+	"Level5": preload("res://Music/Level5.ogg"),
+	"LevelsMenu": preload("res://Music/LevelsMenu.ogg"),
+	"MenuPrincipal": preload("res://Music/MenuPrincipal.ogg"),
+	"26": preload("res://audio/ui/26.ogg"),
+	"27": preload("res://audio/ui/27.ogg"),
+	"back_click": preload("res://audio/ui/back_click.ogg"),
+	"click": preload("res://audio/ui/click.ogg"),
+	"close_game": preload("res://audio/ui/close_game.ogg"),
+	"error": preload("res://audio/ui/error.ogg"),
+	"hover_button": preload("res://audio/ui/hover_button.ogg"),
+	"menu_principal_click": preload("res://audio/ui/menu_principal_click.ogg"),
+	"BrokenWoodBlock": preload("res://Sounds/BrokenWoodBlock.ogg"),
+	"1UP": preload("res://Sounds/1UP.ogg"),
+	"CheckPoint": preload("res://Sounds/CheckPoint.ogg"),
+	"Coin": preload("res://Sounds/Coin.ogg"),
+	"DoubleJumpPowerUp": preload("res://Sounds/DoubleJumpPowerUp.ogg"),
+	"FirePowerUp": preload("res://Sounds/FirePowerUp.ogg"),
+	"Freeze": preload("res://Sounds/Freeze.ogg"),
+	"geiser": preload("res://Sounds/geiser.ogg"),
+	"HawkerShot": preload("res://Sounds/HawrkerShot.ogg"),
+	"HealPowerUp": preload("res://Sounds/HealPowerUp.ogg"),
+	"IcePowerUp": preload("res://Sounds/IcePowerUp.ogg"),
+	"MadBoss": preload("res://Sounds/MadBoss.ogg"),
+	"MadMox": preload("res://Sounds/MadMox.ogg"),
+	"NoTime": preload("res://Sounds/NoTime.ogg"),
+	"Portal": preload("res://Sounds/Portal.ogg"),
+	"ShieldPowerUp": preload("res://Sounds/ShieldPowerUp.ogg"),
+	"ShotImpact": preload("res://Sounds/ShotImpact.ogg"),
+	"SorceravenShot": preload("res://Sounds/SorceravenShot.ogg"),
+	"Walk": preload("res://Sounds/Walk.ogg"),
+	"Water1": preload("res://Sounds/Water1.ogg")
+}
+
 var current_music = ""
 var music_player: AudioStreamPlayer
 
@@ -16,6 +58,14 @@ func _ready():
 	music_player = AudioStreamPlayer.new()
 	music_player.bus = "Música"
 	add_child(music_player)
+	for sound_name in PRELOAD_SOUNDS:
+		if not sounds.has(sound_name):
+			var sound = AudioStreamPlayer.new()
+			sound.stream = PRELOAD_SOUNDS[sound_name]
+			sound.bus = "Efectos"
+			add_child(sound)
+
+			sounds[sound_name] = sound
 	
 	for path in AUDIO_PATHS:
 		_load_ui_sounds(path)
