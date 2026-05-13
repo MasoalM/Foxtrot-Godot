ce2755790dee182b12e8d0dc0a9669d4a1b12f3b
hugoBackup <hugovalls082@gmail.com>
2026-05-13 12:44:05 +0200

    tenemos server!

--- API/APIclient.gd
+++ API/APIclient.gd
@@ -1,21 +1,33 @@
 extends Node
 
-var url_base = "http://localhost/api/"
+var url_base = "https://antiquewhite-cattle-735083.hostingersite.com/api/"
 
 func enviar_resultado(data: Dictionary):
 	var http = HTTPRequest.new()
 	add_child(http)
-	
+
 	var url = url_base + "save_progress.php"
-	
+
 	var json = JSON.stringify(data)
-	var headers = ["Content-Type: application/json"]
-	
-	http.request(url, headers, HTTPClient.METHOD_POST, json)
-	
+
+	var headers = [
+		"Content-Type: application/json"
+	]
+
+	http.request(
+		url,
+		headers,
+		HTTPClient.METHOD_POST,
+		json
+	)
+
 	http.request_completed.connect(_on_request_completed)
 
 
-func _on_request_completed(_result, _response_code, _headers, body):
+func _on_request_completed(result, response_code, headers, body):
+
+	print("HTTP CODE:", response_code)
+
 	var texto = body.get_string_from_utf8()
+
 	print("Respuesta servidor:", texto)
