1d8dbfe06defeed6cddab2849cf76e5033569acc
Sergilanovas22 <133489467+Sergilanovas22@users.noreply.github.com>
2026-05-14 13:24:47 +0200

    proyectilTorreta arreglado

--- Proyectiles/proyectil_torreta.gd
+++ Proyectiles/proyectil_torreta.gd
@@ -1,18 +1,18 @@
 extends Area2D
 
-const dist_max = 180.0
+const dist_max = 1088.0
 var vel_bala = 600.0
-var direccion = Vector2(1, 0)  # ← dirección, la torreta la sobreescribe
+var direccion = Vector2(1, 0)
 var dist = 0.0
-
-func _ready() -> void:
-	pass
 
 func _process(delta: float) -> void:
 	if visible:
-		position += direccion * vel_bala * delta  # ← usa el vector
-		dist += 1
-		if dist > dist_max:
+		var movimiento = direccion * vel_bala * delta
+		
+		position += movimiento
+		dist += movimiento.length() # suma la distancia real recorrida
+		
+		if dist >= dist_max:
 			morir()
 
 func morir():
