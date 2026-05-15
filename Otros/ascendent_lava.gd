07ee8304c0106fb4d24bee360b326c775b4319ab
Sergilanovas22 <133489467+Sergilanovas22@users.noreply.github.com>
2026-05-15 10:20:25 +0200

    detalle nivel 10

--- Otros/ascendent_lava.gd
+++ Otros/ascendent_lava.gd
@@ -3,7 +3,8 @@ extends Area2D
 @export var speed := 80.0
 
 func _process(delta):
-	position.y -= speed * delta
+	if position.y>-5250:
+		position.y -= speed * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
