cff41060a1866757d98b8f9dec4121253facd209
Sergilanovas22 <133489467+Sergilanovas22@users.noreply.github.com>
2026-05-16 22:43:55 +0200

    monedas nivel4

--- Sprites/Coleccionables/frog.gd
+++ Sprites/Coleccionables/frog.gd
@@ -11,8 +11,31 @@ const FROG_DATA := {
 	FrogType.RED:   { "value": 10, "color": Color(1.0, 0, 0, 1.0) },
 }
 
+var base_y := 0.0
+
 func _ready() -> void:
 	sprite.modulate = FROG_DATA[frog_type]["color"]
+
+	base_y = position.y
+	_start_floating()
+
+func _start_floating() -> void:
+	var tween := create_tween()
+	tween.set_loops()
+
+	tween.tween_property(
+		self,
+		"position:y",
+		base_y - 3,
+		0.8
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_property(
+		self,
+		"position:y",
+		base_y + 3,
+		0.8
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -21,8 +44,11 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _collect() -> void:
 	set_deferred("monitoring", false)
+
 	AudioManager.play("Coin", -7.0, 2.0, global_position)
+
 	var tween := create_tween()
+
 	tween.tween_property(sprite, "scale", Vector2(1.6, 1.6), 0.1)
 	tween.tween_property(sprite, "modulate:a", 0.0, 0.15)
 	tween.tween_callback(queue_free)
